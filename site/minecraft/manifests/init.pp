1d0ddf00a77cfa00d514f54698f9cf72189f34ff
iuli72an <88328098+iuli72an@users.noreply.github.com>
2021-08-05 19:09:59 +0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,23 +1,28 @@
 class minecraft {
   file {'/opt/minecraft':
     ensure => directory,
-   }
-   file {'/opt/minecraft/server.jar':
+  }
+  file {'/opt/minecraft/minecraft_server.jar':
     ensure => file,
-    source => https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar
-   }
-   package {'java':
+    source => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.1/minecraft_server.1.12.1.jar',
+    before => Service[‘minecraft’],
+  }
+
+
+  package {‘java’:
     ensure => present,
-   }
-   file {'/opt/minecraft/eula.txt':
+  }
+  file {‘/opt/minecraft/eula.txt’:
     ensure => file,
-    content => 'eula=true',
-   }
-   file {'/etc/systemd/system/minecraft.service':
+    content => ‘eula=true’
+  }
+  file {‘/etc/systemd/system/minecraft.service’:
     ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service',
-   }
-   service {'minecraft':
+    source => ‘puppet:///modules/minecraft/minecraft.service’,
+  }
+  service { ‘minecraft’:
     ensure => running,
     enable => true,
- }
+    require => [Package[‘java’],File[‘/opt/minecraft/eula.txt’],File[‘/etc/systemd/system/minecraft.service’]],
+  }
+}
