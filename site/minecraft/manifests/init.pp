0da12fa18a6ecf247cb4190ebca07c104ff1de2b
iuli72an <88328098+iuli72an@users.noreply.github.com>
2021-08-06 10:27:40 +0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,14 +1,13 @@
 class minecraft (
-  $url = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar'
-  $install_dir = '/srv/minecraft'
+  $url = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.1/minecraft_server.1.12.1.jar',
+  $install_dir = '/opt/minecraft'
 ){
-  file {$install_dir:
+  file { $install_dir:
     ensure => directory,
   }
-  file {"${install_dir}/minecraft_server.1.12.2.jar":
+  file { "${install_dir}/minecraft_server.jar":
     ensure => file,
     source => $url,
-    before => Service['minecraft'],
   }
   package {'java':
     ensure => present,
